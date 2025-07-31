3f9c53e6c355cd27b9b2cb397a9881e32dd5b9bf
Sai Vineeth Grandhi <grandhisaivineeth@gmail.com>
2025-07-31 08:42:37 -0400

    Creating microservices project

--- accounts/src/main/java/com/vineeth/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/vineeth/accounts/service/IAccountsService.java
@@ -9,4 +9,8 @@ public interface IAccountsService {
     void createAccount(CustomerDto customerDto);
 
     CustomerDto fetchAccountDetails(String mobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
 }
