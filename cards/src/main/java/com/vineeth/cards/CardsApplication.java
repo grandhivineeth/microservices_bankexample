571cfccf46ecbd58505bfefee8184d6cd18e1298
Sai Vineeth Grandhi <grandhisaivineeth@gmail.com>
2025-08-08 19:15:55 -0400

    Created cards microservice

--- cards/src/main/java/com/vineeth/cards/CardsApplication.java
+++ cards/src/main/java/com/vineeth/cards/CardsApplication.java
@@ -3,6 +3,7 @@ package com.vineeth.cards;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -18,7 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 						name = "Vineeth",
 						email = "7oMl6@example.com"
 				),
-				license = @io.swagger.v3.oas.annotations.info.License(
+				license = @License(
 						name = "Apache 2.0",
 						url = "https://www.apache.org/licenses/LICENSE-2.0"
 				)
